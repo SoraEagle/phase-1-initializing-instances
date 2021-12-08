3af6323a15cd5279168fa3d53bb3cd131934aff6
Matthew <phillips.matthew829@gmail.com>
2021-12-08 11:35:08 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your code here
+/*
+class Dog{ //Class to create new Dog Objects.
+    #breed; //Declares breed as a private property.
+    constructor(name, breed){ //Constructor for new Dog Objects.
+        this.name = name; //A string.
+        this.#breed = breed; //A String.
+    }
+    //Functions go here.
+    bark(){
+        console.log(`${this.name} barks!`);
+    }
+}*/
+
+//Create classes and constructors for Breakfast, Lunch, and Dinner
+class Breakfast{
+    constructor(food, drink){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch{
+    constructor(salad, soup, drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner{
+    #dessert;
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert;
+    }
+}
